a3116ecfb5e05db4d1d252390c191c4156251d40
aalibrahimi <166450703+aalibrahimi@users.noreply.github.com>
2024-10-09 17:13:50 -0700

    nav ui icon bar installment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
-import Image from "next/image";
+// import { Dock } from "lucide-react";
+import { Dock, DockIcon } from "@/components/ui/dock";
+import { Home, FileText, Github, Linkedin, Twitter, Youtube, Moon } from 'lucide-react';
 
-export default function Home() {
+export default function Page() {
   return (
-   
+    <>
+      {/* Your other page content */}
+      <Dock className="fixed bottom-4 left-1/2 -translate-x-1/2 z-50">
+        <DockIcon>
+          <Home size={24} />
+        </DockIcon>
+        <DockIcon>
+          <FileText size={24} />
+        </DockIcon>
+        <DockIcon>
+          <Github size={24} />
+        </DockIcon>
+        <DockIcon>
+          <Linkedin size={24} />
+        </DockIcon>
+        <DockIcon>
+          <Twitter size={24} />
+        </DockIcon>
+        <DockIcon>
+          <Youtube size={24} />
+        </DockIcon>
+        <DockIcon>
+          <Moon size={24} />
+        </DockIcon>
+      </Dock>
+    </>
   );
 }
